8975cd33254b9e9346e18906d6206f7bd07ca085
BuildTools <arthurpisakhov1@gmail.com>
2019-08-14 21:53:40 -0400

    callback.js mvp

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,24 +27,43 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
+
+getLength(items, (length) => console.log(length));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1])
 }
+
+last(items, (last) => console.log(last));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(3, 4, (sum) => console.log(sum));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(3, 4, (multiple) => console.log(multiple))
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    return cb(true)
+  } else {
+    return cb(false)
+  }
 }
+
+contains('Pencil', items, (item) => console.log(item));
 
 /* STRETCH PROBLEM */
 
